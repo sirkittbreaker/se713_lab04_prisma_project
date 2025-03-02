d6b8976befdc480964dfc7e30b0951b8d73e34d6
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-02 16:46:42 +0700

    handle port

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use("/members", memberRoute);
 app.use("/books", bookRoute);
 app.use("/borrowing", borrowingRoute);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
